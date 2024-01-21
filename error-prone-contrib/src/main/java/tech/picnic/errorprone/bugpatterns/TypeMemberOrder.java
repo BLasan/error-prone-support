eb085af0c4b44834fc25463d9da676bdc31c06b0
Benedek Halasi <benedek.halasi@teampicnic.com>
2024-01-21 13:55:13 +0100

    Plug'n play source code tracking alt

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/TypeMemberOrder.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/TypeMemberOrder.java
@@ -1,43 +1,40 @@
 package tech.picnic.errorprone.bugpatterns;
 
+import static com.google.common.base.Verify.verify;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.errorprone.BugPattern.LinkType.CUSTOM;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
 import static com.google.errorprone.BugPattern.StandardTags.STYLE;
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.joining;
+import static java.util.Objects.requireNonNull;
 import static tech.picnic.errorprone.bugpatterns.util.Documentation.BUG_PATTERNS_BASE_URL;
 
 import com.google.auto.service.AutoService;
 import com.google.auto.value.AutoValue;
 import com.google.common.base.VerifyException;
+import com.google.common.collect.Comparators;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Streams;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.VisitorState;
+import com.google.errorprone.annotations.Var;
 import com.google.errorprone.bugpatterns.BugChecker;
 import com.google.errorprone.fixes.SuggestedFix;
-import com.google.errorprone.fixes.SuggestedFixes;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.util.ASTHelpers;
 import com.google.errorprone.util.ErrorProneToken;
+import com.google.errorprone.util.ErrorProneTokens;
 import com.sun.source.tree.BlockTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
-import com.sun.source.util.TreePath;
 import com.sun.tools.javac.code.Attribute;
-import com.sun.tools.javac.parser.Tokens;
+import com.sun.tools.javac.parser.Tokens.TokenKind;
 import com.sun.tools.javac.util.Position;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Stream;
+import java.util.*;
 import javax.lang.model.element.Modifier;
-import tech.picnic.errorprone.bugpatterns.util.SourceCode;
 
 /**
  * A {@link BugChecker} that flags classes with a non-canonical member order.
@@ -91,52 +88,90 @@ public final class TypeMemberOrder extends BugChecker implements BugChecker.Clas
             }
           });
 
-  /**
-   * Collection of values that are when provided in a {@link SuppressWarnings} annotation of a
-   * member, this BugChecker will not sort it.
-   */
-  private static final ImmutableSet<String> RECOGNIZED_SUPPRESSIONS =
-      ImmutableSet.of("all", TypeMemberOrder.class.getSimpleName());
-
   /** Instantiates a new {@link TypeMemberOrder} instance. */
   public TypeMemberOrder() {}
 
   @Override
   public Description matchClass(ClassTree tree, VisitorState state) {
-    //    ImmutableList<TypeMember> typeMembers =
-    //        getTypeMembersWithComments(tree, state).stream()
-    //            .filter(typeMember -> canBeSorted(typeMember.tree()))
-    //            .collect(toImmutableList());
-    //
-    //    ImmutableList<TypeMember> sortedTypeMembers =
-    //        ImmutableList.sortedCopyOf(
-    //            comparing(TypeMember::tree, BY_PREFERRED_TYPE_MEMBER_ORDER), typeMembers);
-    //
-    //    if (typeMembers.equals(sortedTypeMembers)) {
-    //      return Description.NO_MATCH;
-    //    }
-    //
-    //    return buildDescription(tree)
-    //        .addFix(replaceTypeMembers(typeMembers, sortedTypeMembers, state))
-    //        .build();
-    ImmutableList<Tree> typeMemberTrees =
-        tree.getMembers().stream().filter(TypeMemberOrder::canBeSorted).collect(toImmutableList());
+    ImmutableList<Tree> sortableMembers =
+        tree.getMembers().stream().filter(m -> canMove(m, state)).collect(toImmutableList());
 
-    ImmutableList<Tree> sortedTypeMemberTrees =
-        ImmutableList.sortedCopyOf(BY_PREFERRED_TYPE_MEMBER_ORDER, typeMemberTrees);
-
-    if (typeMemberTrees.equals(sortedTypeMemberTrees)) {
+    if (Comparators.isInOrder(sortableMembers, BY_PREFERRED_TYPE_MEMBER_ORDER)) {
       return Description.NO_MATCH;
     }
 
-    ImmutableList<TypeMember> sortedTypeMembers =
-        sortedTypeMemberTrees.stream()
-            .map(member -> getTypeMembersWithComments(tree, member, state))
-            .collect(toImmutableList());
+    int classBodyStart = getBodyStartPos(tree, state);
+    if (classBodyStart == Position.NOPOS) {
+      /*
+       * We can't determine the class body's start position in the source code. This generally means
+       * that (part of) its code was generated. Even if the source code for a subset of its members
+       * is available, dealing with this edge case is not worth the trouble.
+       */
+      return Description.NO_MATCH;
+    }
 
-    SuggestedFix fix = replaceTypeMembers(typeMemberTrees, sortedTypeMembers, state);
+    return describeMatch(tree, sortClassMembers(classBodyStart, sortableMembers, state));
+  }
 
-    return buildDescription(tree).addFix(fix).build();
+  private boolean canMove(Tree tree, VisitorState state) {
+    return state.getEndPosition(tree) != Position.NOPOS && !isSuppressed(tree, state);
+  }
+
+  private static int getBodyStartPos(ClassTree clazz, VisitorState state) {
+    CharSequence sourceCode = state.getSourceCode();
+    int classStart = ASTHelpers.getStartPosition(clazz);
+    int classEnd = state.getEndPosition(clazz);
+    if (sourceCode == null || classStart == Position.NOPOS || classEnd == Position.NOPOS) {
+      return Position.NOPOS;
+    }
+
+    /*
+     * We return the source code position that follows the first left brace after the `class`
+     * keyword.
+     */
+    // XXX: !! Doesn't work for interfaces. How to make exhaustive? For records we'd even need
+    // special handling. (But maybe we should support only classes and interfaces for now? Enums
+    // have other considerations!)
+    return ErrorProneTokens.getTokens(
+            sourceCode.subSequence(classStart, classEnd).toString(), classStart, state.context)
+        .stream()
+        .dropWhile(t -> t.kind() != TokenKind.CLASS)
+        .dropWhile(t -> t.kind() != TokenKind.LBRACE)
+        .findFirst()
+        .map(ErrorProneToken::endPos)
+        .orElse(Position.NOPOS);
+  }
+
+  /**
+   * Suggests a different way of ordering the given class members.
+   *
+   * @implNote For each member, this method tracks the source code between the end of the definition
+   *     of the member that precedes it (or the start of the class body if there is no such member)
+   *     and the end of the definition of the member itself. This subsequently enables moving
+   *     members around, including any preceding comments and Javadoc. This approach isn't perfect,
+   *     and may at times move too much code or documentation around; users will have to manually
+   *     resolve this.
+   */
+  private static SuggestedFix sortClassMembers(
+      int classBodyStart, ImmutableList<Tree> members, VisitorState state) {
+    List<TypeMember> membersWithSource = new ArrayList<>();
+
+    @Var int start = classBodyStart;
+    for (Tree member : members) {
+      int end = state.getEndPosition(member);
+      verify(end != Position.NOPOS && start < end, "Unexpected member end position");
+      membersWithSource.add(new AutoValue_TypeMemberOrder_TypeMember(member, start, end));
+      start = end;
+    }
+
+    CharSequence sourceCode = requireNonNull(state.getSourceCode(), "Source code");
+    return Streams.zip(
+            membersWithSource.stream(),
+            membersWithSource.stream()
+                .sorted(comparing(TypeMember::tree, BY_PREFERRED_TYPE_MEMBER_ORDER)),
+            (original, replacement) -> original.replaceWith(replacement, sourceCode))
+        .reduce(SuggestedFix.builder(), SuggestedFix.Builder::merge, SuggestedFix.Builder::merge)
+        .build();
   }
 
   private static boolean isStatic(VariableTree variableTree) {
@@ -152,107 +187,21 @@ public final class TypeMemberOrder extends BugChecker implements BugChecker.Clas
     return ASTHelpers.getSymbol(methodTree).isConstructor();
   }
 
-  private static boolean canBeSorted(Tree tree) {
-    if (hasRecognizedSuppressWarnings(tree)) {
-      return false;
-    }
-    return tree instanceof VariableTree
-        || (tree instanceof MethodTree && !ASTHelpers.isGeneratedConstructor((MethodTree) tree))
-        || tree instanceof BlockTree
-        || tree instanceof ClassTree;
-  }
-
-  private static Boolean hasRecognizedSuppressWarnings(Tree tree) {
-    return Optional.ofNullable(
-            ASTHelpers.getAnnotationWithSimpleName(
-                ASTHelpers.getAnnotations(tree), "SuppressWarnings"))
-        .flatMap(
-            suppressWarningsTree ->
-                ASTHelpers.getAnnotationMirror(suppressWarningsTree)
-                    .getElementValues()
-                    .values()
-                    .stream()
-                    // Assuming SuppressWarnings has a single member (`String[] value()`)
-                    .findAny())
-        .map(
-            annotationValue ->
-                ((Attribute.Array) annotationValue)
-                    .getValue().map(attr -> (String) attr.getValue()).stream()
-                        .anyMatch(RECOGNIZED_SUPPRESSIONS::contains))
-        .orElse(false);
-  }
-
-  private static SuggestedFix replaceTypeMembers(
-      ImmutableList<Tree> typeMembers,
-      ImmutableList<TypeMember> replacementTypeMembers,
-      VisitorState state) {
-    return Streams.zip(
-            typeMembers.stream(),
-            replacementTypeMembers.stream(),
-            (original, replacement) -> replaceTypeMember(original, replacement, state))
-        .reduce(SuggestedFix.builder(), SuggestedFix.Builder::merge, SuggestedFix.Builder::merge)
-        .build();
-  }
-
-  private static SuggestedFix replaceTypeMember(
-      Tree original, TypeMember replacement, VisitorState state) {
-    /* Technically this check is not necessary, but it avoids redundant replacements. */
-    if (original.equals(replacement.tree())) {
-      return SuggestedFix.emptyFix();
-    }
-
-    String replacementSource =
-        Stream.concat(
-                replacement.comments().stream(),
-                Stream.of(SourceCode.treeToString(replacement.tree(), state)))
-            .collect(joining(System.lineSeparator()));
-    return SuggestedFixes.replaceIncludingComments(
-        TreePath.getPath(state.getPath(), original), replacementSource, state);
-  }
-
-  /** Returns the type's members with their comments. */
-  private static TypeMember getTypeMembersWithComments(
-      ClassTree tree, Tree member, VisitorState state) {
-    return new AutoValue_TypeMemberOrder_TypeMember(
-        member, getTypeMemberComments(tree, member, state));
-  }
-
-  private static ImmutableList<String> getTypeMemberComments(
-      ClassTree tree, Tree member, VisitorState state) {
-    int typeStart = ASTHelpers.getStartPosition(tree);
-    int typeEnd = state.getEndPosition(tree);
-    int memberStart = ASTHelpers.getStartPosition(member);
-    int memberEnd = state.getEndPosition(member);
-    if (typeStart == Position.NOPOS
-        || typeEnd == Position.NOPOS
-        || memberStart == Position.NOPOS
-        || memberEnd == Position.NOPOS) {
-      /* Source code details appear to be unavailable. */
-      return ImmutableList.of();
-    }
-
-    // TODO: Move identifying "previous member end position" to an outer loop,
-    //  Loop once and identify for all members
-    // TODO: Check if this handles properly comments on the first member.
-    Optional<Integer> previousMemberEndPos =
-        state.getOffsetTokens(typeStart, typeEnd).stream()
-            .map(ErrorProneToken::endPos)
-            .takeWhile(endPos -> endPos < memberStart)
-            .reduce((earlierPos, laterPos) -> laterPos);
-
-    List<ErrorProneToken> typeMemberTokens =
-        state.getOffsetTokens(previousMemberEndPos.orElse(memberStart), memberEnd);
-
-    // TODO: double check this .get(0)
-    return typeMemberTokens.get(0).comments().stream()
-        .map(Tokens.Comment::getText)
-        .collect(toImmutableList());
-  }
-
   @AutoValue
   abstract static class TypeMember {
     abstract Tree tree();
 
-    abstract ImmutableList<String> comments();
+    abstract int startPosition();
+
+    abstract int endPosition();
+
+    SuggestedFix replaceWith(TypeMember other, CharSequence fullSourceCode) {
+      return equals(other)
+          ? SuggestedFix.emptyFix()
+          : SuggestedFix.replace(
+              startPosition(),
+              endPosition(),
+              fullSourceCode.subSequence(other.startPosition(), other.endPosition()).toString());
+    }
   }
 }
